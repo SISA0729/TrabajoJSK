dd2511b1cae7de79849e5958502a8591ecefd0e5
ismael.sisa <ismael.sisa@educa.madrid.org>
2024-05-14 12:05:42 +0200

    SISA

--- src/main/java/com/example/trabajojsk/App.java
+++ src/main/java/com/example/trabajojsk/App.java
@@ -1,7 +1,9 @@
 package com.example.trabajojsk;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,11 +12,12 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("sisa");
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/trabajojsk/inicio.fxml"));
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/trabajojsk/Controller.java
+++ src/main/java/com/example/trabajojsk/Controller.java
@@ -1,14 +1,52 @@
 package com.example.trabajojsk;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import org.controlsfx.control.action.Action;
 
-public class Controller {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
     @FXML
-    private Label welcomeText;
+    private TextField textUsuario;
+    private TextField textContraseña;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("johan manda");
+    private Button botonIniciar;
+
+    @FXML
+    private void eventkey(KeyEvent event){
+
+    }
+
+    @FXML
+    private void eventAction(ActionEvent event) throws IOException {
+        Object o = event.getSource();
+        Node node = (Node) o;
+        Scene scene1 = node.getScene();
+        Window window = scene1.getWindow();
+        Stage stage = (Stage) window;
+
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/trabajojsk/pestaña.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
