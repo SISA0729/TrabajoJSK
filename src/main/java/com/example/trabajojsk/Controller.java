d9a3eb5b91f1c150d42fd97e1ba9311d864003e6
ismael.sisa <ismael.sisa@educa.madrid.org>
2024-05-15 13:06:14 +0200

    SISA

--- src/main/java/com/example/trabajojsk/Controller.java
+++ src/main/java/com/example/trabajojsk/Controller.java
@@ -8,6 +8,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
@@ -21,6 +22,9 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     @FXML
+    private Hyperlink link;
+
+    @FXML
     private TextField textUsuario;
     private TextField textContrasena;
 
@@ -31,7 +35,19 @@ public class Controller implements Initializable {
     private void eventkey(KeyEvent event){
 
     }
+    @FXML
+    private void eventLink(ActionEvent event) throws IOException {
 
+        Object s = event.getSource();
+        Node node = (Node) s;
+        Scene scene1 = node.getScene();
+        Window window = scene1.getWindow();
+        Stage stage = (Stage) window;
+
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/trabajojsk/registro.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
     @FXML
     private void eventAction(ActionEvent event) throws IOException {
 
